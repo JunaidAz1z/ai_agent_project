8e806e5a49d4c613eac7c2941938dfbc59780c6e
Junaid Aziz <junaidaziz841@gmail.com>
2026-05-18 22:20:10 +0500

    LangChain Messages, Prompts, Chains Covered

--- LangChainFile.py
+++ LangChainFile.py
@@ -171,3 +171,184 @@
 #     print(response.content)
 
 
+
+
+
+
+                                    #Chains PIPE OPERATOR
+
+
+# from langchain_groq import ChatGroq
+# from langchain_core.prompts import ChatPromptTemplate
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+# template = ChatPromptTemplate.from_messages([
+#     ("system","You are a senior developer"),
+#     ("human", "Tell me one line about {topic}")
+# ])
+
+# #without chains
+# # prompt = template.invoke({"topic":"python"})
+# # response = llm.invoke(prompt)
+# # print(response.content)
+
+# #with chains
+# chains = template | llm
+# response = chains.invoke({"topic":"python"})
+# print(response)
+
+
+#Chain + Output_parser
+
+# from langchain_groq import ChatGroq
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# llm = ChatGroq(model="llama-3.3-70b-versatile")
+# parser = StrOutputParser()
+# template = ChatPromptTemplate.from_messages([
+#     ("system","You are a senior developer"),
+#     ("human", "Tell me one line about {topic}")
+# ])
+
+
+# chains = template | llm | parser
+# response = chains.invoke({"topic":"python"})
+# print(response)
+
+
+#Chain with loop 
+
+# from langchain_groq import ChatGroq
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# llm = ChatGroq(model="llama-3.3-70b-versatile")
+# parser = StrOutputParser()
+
+# template = ChatPromptTemplate.from_messages([
+#     ("system", "You are a geography expert."),
+#     ("human", "What is the capital of {country}?")
+# ])
+
+# chain = template | llm | parser
+
+# # Multiple countries!
+# countries = ["Pakistan", "Turkey", "Japan"]
+
+# for country in countries:
+#     response = chain.invoke({"country": country})
+#     print(f"{country}: {response}")
+
+
+#Ex1
+
+# from langchain_groq import ChatGroq
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# llm = ChatGroq(model="llama-3.3-70b-versatile")
+# parser = StrOutputParser()
+# template = ChatPromptTemplate.from_messages([
+#     ("system", "You are a comedian"),
+#     ("human", "Tell me a joke about {topic}")
+# ])
+
+# chain = template | llm | parser
+# topics = ["cricket", "coding", "karachi"]
+# for topic in topics:
+#     response = chain.invoke({"topic": topic})
+#     print(f"{topic}: {response}")
+
+#Ex2
+
+# from langchain_groq import ChatGroq
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# llm = ChatGroq(model="llama-3.3-70b-versatile")
+# parser = StrOutputParser()
+# template = ChatPromptTemplate.from_messages([
+#     ("system", "You are a {language} expert"),
+#     ("human", "Write a {type} program in {language} that {task}")
+# ])
+
+# chain = template | llm | parser
+# response = chain.invoke({"language":"Python","type":"simple","task":"prints numbers 1 to 10"})
+# print(response)
+
+#Ex3
+
+# from langchain_groq import ChatGroq
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# llm = ChatGroq(model="llama-3.3-70b-versatile")
+# parser = StrOutputParser()
+# template = ChatPromptTemplate.from_messages([
+#     ("system", "You are a news reporter"),
+#     ("human", "Write 3 news headlines about {topic} in {country}")
+# ])
+
+# chain1 = template | llm | parser
+# response1 = chain1.invoke({"topic":"cricket", "country":"Pakistan"})
+# print(response1)
+
+# chain2 = template | llm | parser
+# response2 = chain2.invoke({"topic":"technology", "country":"USA"})
+# print(response2)
+
+
+#Ex4
+
+# from langchain_groq import ChatGroq
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# llm = ChatGroq(model="llama-3.3-70b-versatile")
+# parser = StrOutputParser()
+# template = ChatPromptTemplate.from_messages([
+#     ("system", "You are a senior dev"),
+#     ("human", "What is {language} best used for? Answer in one line.")
+# ])
+
+# chain = template | llm | parser
+# languages = ["Python", "JavaScript", "Java", "C++", "Go"]
+# for language in languages:
+
+#     response = chain.invoke({"language": language})
+#     print(f"{language}:     {response}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
